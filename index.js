6b20029b36629aa446df59db046ccfb243c4058d
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-02-22 14:28:39 +0600

    some html file send

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express=require("express")
+const express=require("express");
+const fs=require('fs');
 
 const app=express();
 const port=4000;
@@ -9,10 +10,45 @@ const port=4000;
 // }
 // route.get("/",anyfunction);
 
-
+// some route 
 app.get("/",/*handle request*/(req,res)=>{
-    res.send("Hello Hasan")
+    fs.readFile("./pages/index.html",(err,data)=>{
+        if(err){
+            console.log("err",err);
+            res.send("<h1>somthing wrong</h1>")
+        }
+        else{
+            res.write(data)
+            res.end()
+        }
+    });
 })
+app.get("/about",/*handle request*/(req,res)=>{
+    fs.readFile("./pages/about.html",(err,data)=>{
+        if (err){
+            console.log("err",err);
+            res.send("<h1>somthing wrong</h1>")
+        }
+        else{
+            res.write(data);
+            res.end();
+        }
+    })
+})
+app.get("/service",/*handle request*/(req,res)=>{
+    fs.readFile("./pages/services.html",(err,data)=>{
+        if(err){
+            console.log("err",err)
+            res.send("<h1>somthing wrong</h1>")
+        }
+        else{
+            res.write(data)
+            res.end();
+        }
+    })
+})
+
+// some route 
 app.listen(port,()=>{
     console.log("my server is running")
 })
