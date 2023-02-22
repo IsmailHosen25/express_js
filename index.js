8d13780a5e211fa1336583a0298ea3d07ce4ae43
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-02-22 13:40:26 +0600

    nodemon installed  and route, heandler expleind

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express=require("express")
 
 const app=express();
 const port=4000;
+
+// handler function 
+//  function hello(req,res,next){
+// res.send("Hello!!!")
+// }
+// route.get("/",anyfunction);
+
+
+app.get("/",/*handle request*/(req,res)=>{
+    res.send("Hello Hasan")
+})
 app.listen(port,()=>{
     console.log("my server is running")
 })
